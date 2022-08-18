6fc4975707dd064459c0e8aa2d23a3955189d570
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-08-18 16:37:50 -0300

    requisito 2 realizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Table from './components/ Table';
 import FormFiltro from './components/FormFiltro';
@@ -6,13 +6,27 @@ import FormOrdenar from './components/FormOrdenar';
 import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
+  const [filterByName, setfilterByName] = useState({ name: '' });
+  const { name } = filterByName;
+
+  function handleChange({ target }) {
+    const { value } = target;
+    setfilterByName({ name: value });
+  }
+
   return (
     <PlanetsProvider>
       <h1>Starwars Planets Search</h1>
-      <input type="text" />
+      <input
+        type="text"
+        data-testid="name-filter"
+        name="name"
+        value={ name }
+        onChange={ handleChange }
+      />
       <FormFiltro />
       <FormOrdenar />
-      <Table />
+      <Table filterByName={ filterByName } />
     </PlanetsProvider>
   );
 }
--- src/components/ Table.js
+++ src/components/ Table.js
@@ -1,10 +1,9 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import PlanetsContext from '../context/PlanetsContext';
 
-function Table() {
-  // const [infoPlanets, setInfoPlanets] = useState(fetchPlanets());
+function Table({ filterByName }) {
   const planets = useContext(PlanetsContext);
-  console.log(planets[0]);
   return (
     <table>
       <tr>
@@ -15,26 +14,34 @@ function Table() {
       </tr>
       {
         planets.length !== 0
-        && planets.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.created}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.films}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.population}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))
+        && planets.filter(
+          (planet) => planet.name.toLowerCase()
+            .includes(filterByName.name.toLowerCase()),
+        )
+          .map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.created}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.films}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.population}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))
       }
     </table>
   );
 }
+
+Table.propTypes = {
+  filterByName: PropTypes.func.isRequired,
+};
 
 export default Table;
